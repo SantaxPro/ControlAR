b089d1aea8b8fc4799b1fee384fb862e925be015
Daniel Chavez <daniel.chavez.epet20@gmail.com>
2022-06-25 13:42:26 -0300

    Agrego el archivo theme.js con los estilos generales de la aplicacion (colores, etc) y el componente styledbutton que acepta props y cambia el estilo del boton

---
+++ src/components/StyledButton.jsx
@@ -0,0 +1,54 @@
+import {View, Button, Text, StyleSheet } from 'react-native';
+import theme from './theme';
+
+const styles = StyleSheet.create({
+    buttonStandard: {
+        width: 125,
+        height: 35, 
+        backgroundColor: theme.colors.primaryGrey,
+        borderRadius: 50,
+        justifyContent: 'center',
+        color: theme.colors.white,
+    }, 
+    transparentButton: {
+        width: 125,
+        height: 35, 
+        borderRadius: 50,
+        color: theme.colors.secondaryGrey,
+        justifyContent: 'center',
+        borderColor: theme.colors.secondaryGrey,
+        borderWidth: .5,
+    },
+    thinButton: {
+        width: 50,
+        height: 15, 
+        borderRadius: 50,
+        color: theme.colors.white,
+        justifyContent: 'center',
+        backgroundColor: theme.colors.primaryGrey,
+    },
+    thickButton: {
+        width: 80,
+        height: 30, 
+        borderRadius: 50,
+        color: theme.colors.white,
+        justifyContent: 'center',
+        backgroundColor: theme.colors.primaryGrey,
+    },
+});
+
+export default function StyledButton(standard, text, transparent, thin, thick, type) {
+    const buttonStyles = [
+        styles.buttonStandard,
+        type == 'transparent' && styles.transparentButton,
+        type == 'thin' && styles.thinButton,
+        type == 'thick' && styles.thickButton,
+    ]
+    return (
+        <Button style={buttonStyles}>
+            <Text>{text}</Text> 
+        </Button>
+        );
+}
+
+//Crear el styled text
---
+++ src/components/theme.js
@@ -0,0 +1,23 @@
+const theme = {
+    colors: {
+        screensBackground: '#F2F1F0',
+        primaryGrey: '#2A2C33',
+        secondaryGrey: '#2D2E33',
+        lightGrey: '#EFEFF0',
+        white: '#F5F5F4',
+    },
+    fontSizes: {
+        buttonTextSize: '12', 
+        titleTextSize: '14'
+    },
+    fonts: {
+        main: 'roboto',
+    },
+    fontWeight: {
+        normal: '400',
+        medium: '550',
+    },
+}
+
+export default theme;
+
