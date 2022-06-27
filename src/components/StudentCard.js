a475b18c71c4b5cb6f2f95b1e3c17a830f0c3ef7
Daniel Chavez <daniel.chavez.epet20@gmail.com>
2022-06-26 23:25:39 -0300

    Agrego TailwindCSS al proyecto

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -1,11 +1,12 @@
 import {View , Text, StyleSheet, StatusBar, FlatList} from 'react-native'
 import theme from '../UI/theme'
+import StyledButton from './StyledButton'
 
 export default function StudentCard(props){
     return(
         <View style={styles.container}>
             <Text>{ props.student_name + " " +  props.student_lastname}</Text>
-
+            <StyledButton type="red" />
         </View>
     )
 }
