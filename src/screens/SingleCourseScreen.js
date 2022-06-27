5d8050b89a041cc6aebf6e2255d87970cf0fd6ed
Daniel Chavez <daniel.chavez.epet20@gmail.com>
2022-06-26 22:16:18 -0300

    Avances en el routing, cambios al archivo temporal de data, creacion de pantallas y linkeo entre ellas Mas cambios varios

--- src/screens/SingleCourseScreen.js
+++ src/screens/SingleCourseScreen.js
@@ -1,10 +1,15 @@
 import react from 'react'
 import {View, Text, StyleSheet, StatusBar} from 'react-native'
+import data from '../data/testdata'
 
-export default function SingleCourseScreen({ navigation }) {
+export default function SingleCourseScreen({route,  navigation }) {
+    const { courseId } = route.params;
+
     return (
         <View style={styles.container}>
-            <Text>Curso pantalla</Text>
+            <Text>
+                
+            </Text>
         </View>
     )
 }
