10bd61d536b04e1fe37b4c26a307ecde0dd0a108
Daniel Chavez <daniel.chavez.epet20@gmail.com>
2022-06-28 21:22:08 -0300

    Se implementa el funcionamiento basico de el sistema de asistencia, falta aplicar estilos y utilizar localStorage para almacenar el objeto que sera enviado a la planilla

--- src/screens/AttendanceScreen.js
+++ src/screens/AttendanceScreen.js
@@ -1,12 +1,49 @@
 import {View, Text, StyleSheet, FlatList, Button} from "react-native";
 import {useState} from "react";
+import data from '../data/testdata'
 
 export default function AttendanceScreen({navigation, route}) {
-    console.log(route)
-    const [students, setStudents] = useState([]);
+
+    const [students, setStudents] = useState(route.params.array);
+    const [attendance, setAttendance] = useState([]);
+
     return(
         <View>
-            <Text>Curso: {route.params.courseId}</Text>
+            <FlatList 
+            data={students}
+            renderItem={({item}) => {return (
+            <View>
+                <Text>Nombre alumno: {item.student_name}</Text>
+                <Text>Apellido alumno: {item.student_lastname}</Text>
+                <Button title="Asistió" onPress={() => {
+
+                    var new_assist_entry = {
+                        student_fullname: item.student_name + " " + item.student_lastname,
+                        student_id: item.student_id,
+                        student_attendance: true
+                    }
+                    setAttendance([...attendance, new_assist_entry])
+                    
+                    }}/>
+                <Button title="No Asistió" onPress={() => {
+
+                    var today = new Date();
+
+                    let date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+
+                    var new_assist_entry = {
+                        student_fullname: item.student_name + " " + item.student_lastname,
+                        student_id: item.student_id,
+                        student_attendance: false,
+                        student_attendance_date: date
+                    };
+                    
+                    setAttendance([...attendance, new_assist_entry])
+                    }}/>
+                <Button title="ver" onPress={() => {console.log(attendance)}} />
+            </View>
+            )}}
+            />
         </View>
     )
 }
