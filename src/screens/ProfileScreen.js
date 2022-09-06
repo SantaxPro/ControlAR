f4c6621dbb3133c7d60d42b27cd54d605fd6347c
Daniel Chavez <daniel.chavez.epet20@gmail.com>
2022-09-06 15:36:21 -0300

    Implementado el chequeo de la verificacion de email.

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,44 +1,59 @@
 import { View, Text, StyleSheet, Image, Pressable } from "react-native";
 import StyledButton from "../components/Buttons/StyledButton";
 import theme from "../UI/theme";
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons } from "@expo/vector-icons";
+import { Octicons } from "@expo/vector-icons";
 
 //Pantala de configuracion
 export default function ProfileScreen(props) {
   console.log(props);
   const logOut = () => {
-    props.route.params.logout()
-    }
+    props.route.params.logout();
+  };
   return (
     <View style={styles.mainContainer}>
-        <View style={styles.container}>
-
-          <View style={styles.horizontal}>
-            <Image
-              style={styles.profile}
-              source={props.route.params.user.picture}
-            />
-            <Text style={styles.title}>{props.route.params.user.nickname}</Text>
-          </View>
-
-          <View style={styles.horizontal}>
-            <Text style={styles.subtitle}>{props.route.params.user.name}</Text>
-          </View>
-
-          <View style={styles.horizontal}>
-            <MaterialIcons name="verified" size={24} color={theme.colors.blue} />
-            <Text style={[styles.subtitle, {color: "hsla(0, 0%, 22%, 0.90)",}]}>Verificado</Text>
-          </View>
-
-            <View style={styles.horizontal}>
-                <Pressable onPress={logOut} style={styles.horizontal}>
-                    <MaterialIcons name="logout" size={24} color="#EE6161" />
-                    <Text style={[styles.subtitle, {color: '#EE6161',}]}>
-                        Cerrar sesión
-                    </Text>
-                </Pressable>
-            </View>
+      <View style={styles.container}>
+        <View style={styles.horizontal}>
+          <Image
+            style={styles.profile}
+            source={props.route.params.user.picture}
+          />
+          <Text style={styles.title}>{props.route.params.user.given_name}</Text>
         </View>
+
+        <View style={styles.horizontal}>
+          <Text style={styles.subtitle}>{props.route.params.user.email}</Text>
+        </View>
+
+        <View style={styles.horizontal}>
+          {props.route.params.user.email_verified ? (
+            <>
+              <MaterialIcons
+                name="verified"
+                size={24}
+                color={theme.colors.blue}
+              />
+              <Text style={styles.subtitle}>Verificado</Text>
+            </>
+          ) : (
+            <>
+              <Octicons name="unverified" size={24} color="#EE6161" />
+              <Text style={[styles.subtitle, { color: "#EE6161" }]}>
+                No verificado
+              </Text>
+            </>
+          )}
+        </View>
+
+        <View style={styles.horizontal}>
+          <Pressable onPress={logOut} style={styles.horizontal}>
+            <MaterialIcons name="logout" size={24} color="#EE6161" />
+            <Text style={[styles.subtitle, { color: "#EE6161" }]}>
+              Cerrar sesión
+            </Text>
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 }
