f041ee554e14e82f29a574868ea21348b6426c6b
Daniel Chavez <daniel.chavez.epet20@gmail.com>
2022-10-11 18:42:13 -0300

    Inicializacion Archivo firebase creado y añadido a gitignore

---
+++ src/database/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAFyNfjzqVYwQV9FZhnyKqRXWTaZOSIHQ",
+  authDomain: "quick-attend.firebaseapp.com",
+  projectId: "quick-attend",
+  storageBucket: "quick-attend.appspot.com",
+  messagingSenderId: "486816776365",
+  appId: "1:486816776365:web:0f5521f15ee1699cdbc624"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
