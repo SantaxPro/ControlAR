83e498e5d923c26eabff94e4003f9335daf57ff9
Daniel Chavez <daniel.chavez.epet20@gmail.com>
2022-10-13 14:15:43 -0300

    Seleccion de curso al agregar a un alumno Libreria react-native-select-dropdown agregada

--- src/views/Students/Students.js
+++ src/views/Students/Students.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { View, Text, FlatList, Button, Modal, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  Button,
+  Modal,
+  TextInput,
+  Switch,
+  Pressable,
+} from "react-native";
 import { collection, where, query, onSnapshot } from "firebase/firestore";
 import { db } from "../../database/firebase";
 import { useAuth } from "../../context/authContext";
-import useOperations from "../../context/operationsContext";
-
+import useOperations, { useCourses } from "../../context/operationsContext";
+import SelectDropdown from "react-native-select-dropdown";
 function Students() {
   const [students, setStudents] = React.useState([]);
   const [modalVisible, setModalVisible] = React.useState(false);
@@ -46,7 +55,12 @@ function Students() {
         <Button title="añadir alumno" onPress={handlePress} />
         <FlatList
           data={students}
-          renderItem={({ item }) => <Text>{item.name}{item.lastname}</Text>}
+          renderItem={({ item }) => (
+            <Text>
+              {item.name}
+              {item.lastname}
+            </Text>
+          )}
           keyExtractor={(item) => item.id}
         />
       </View>
@@ -55,14 +69,21 @@ function Students() {
 }
 
 const AddStudentModal = ({ visible, setVisible }) => {
+  //STATES
   const [name, setName] = React.useState("");
   const [lastName, setLastName] = React.useState("");
+  const [selectedCourse, setSelectedCourse] = React.useState({
+    id: "",
+    name: "",
+  });
 
+  //CUSTOM HOOKS
   const { user } = useAuth();
+  const { courses } = useCourses();
   const { createStudent } = useOperations();
 
   const handlePress = () => {
-    createStudent(user.uid, name, lastName, "");
+    createStudent(user.uid, name, lastName, selectedCourse);
     setVisible();
   };
 
@@ -76,7 +97,33 @@ const AddStudentModal = ({ visible, setVisible }) => {
           value={lastName}
           onChangeText={(text) => setLastName(text)}
         />
+
+        <Text>Seleccione curso</Text>
+        <SelectDropdown
+        data={courses}
+        onSelect={(selectedItem, index) => {
+          let course_view = {
+            name: selectedItem.name,
+            id: selectedItem.id,
+          }
+          setSelectedCourse(course_view)
+        }}
+        buttonTextAfterSelection={(selectedItem, index) => {
+          return selectedItem.name;
+        }}
+        rowTextForSelection={(item, index) => {
+          return item.name;
+        }}
+        
+        defaultButtonText="Cursos"
+      />
         <Button title="añadir" onPress={handlePress} />
+        <Button
+          title="ver"
+          onPress={() => {
+            console.log(selectedCourse);
+          }}
+        />
         <Button title="cancelar" onPress={setVisible} />
       </View>
     </Modal>
