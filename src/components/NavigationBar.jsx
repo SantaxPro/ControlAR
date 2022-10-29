1e24f2e82a44bc24069b01f2252071001d7f90c6
santi <santiago.cappelluti@hotmail.com>
2022-10-28 21:24:27 -0300

    libreria "react icons" agregada, pantalla de perfil, boton para volver hacia atras, boton para cerrar sesión, y mas ....

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 import { NavLink } from "./NavLink";
+import {AiOutlineArrowLeft} from "react-icons/ai";
 
 export const NavigationBar = () => {
   const { user } = UserAuth();
@@ -12,12 +13,12 @@ export const NavigationBar = () => {
   };
   return (
     <nav className="flex rounded-b-md  container  mx-auto bg-gray-400 h-16 p-2 items-center justify-end">
-      <span className="rounded-full mr-auto w-10 h-10 bg-gray-700">
+      <span className="mr-auto w-10 h-10 bg-gray-700 rounded-full">
         <img
           onClick={redirection}
           src={user?.photoURL}
           alt="pfp"
-          className="w-full h-full bg-cover"
+          className="w-full h-full bg-cover rounded-full"
         />
       </span>
       <NavLink to="courses" />
@@ -25,3 +26,14 @@ export const NavigationBar = () => {
     </nav>
   );
 };
+export const EmptyNavigationBar = () => {
+  const navigate  = useNavigate();
+  const goBack = () =>{
+    navigate(-1);
+  };
+  return (
+    <nav className="flex rounded-b-md  container  mx-auto bg-gray-400 h-16 p-2 items-center justify-end">
+      <AiOutlineArrowLeft className="mr-auto w-8 h-8 ml-4" onClick={goBack}/>
+    </nav>
+  );
+}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
+import {Button} from "../components/Button";
+import {EmptyNavigationBar} from "../components/NavigationBar";
+
 
 export default function ProfileScreen () {
-const {user}=UserAuth()
+const {LogOut, user}=UserAuth()
 return (
-<div className="flex-col  mx-10 ">
- <div className=" items-center rounded-full mr-auto w-20 h-20"> 
-    <img src={user?.photoURL}  alt="pfp" className="w-full h-full bg-cover"/>
+<div className="flex flex-col items-center">
+ <EmptyNavigationBar/>
+ <div className="w-20 h-20 "> 
+    <img src={user?.photoURL}  alt="pfp" className="w-full h-full rounded-full"/>
  </div> 
-
- Nombre:{user.displayName}
+ <p className="m-5">Nombre: {user.displayName}</p>
  
- Email:{user.email}
-   
+ <p className="m-5"> Email: {user.email}</p>
+ <Button className="bg-red-500" title={"Cerrar Sesión"} onClick={()=>{LogOut()}} />
 </div>
-
-
-
 )
 }
 
